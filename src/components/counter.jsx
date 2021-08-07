76874c01b6e4fe0bd13cdb0533f37cf755f60964
Filip Murgovski <murgovski@gmail.com>
2021-08-07 13:02:35 +0200

    update

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,41 +2,40 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 4,
-        tags: ['tag1', 'tag2', 'tag3']
+        count: 0,
+
     };
 
-    renderTags(){
-        if (this.state.tags.length === 0) return <h1>There is no tags!</h1> 
+    increment = () => {
+        this.setState({ count: this.state.count + 1, });
+    }
+    substraction = () => {
+        this.setState({ count: this.state.count - 1, });
+    }
+    resetRow = () => {
+        this.setState({ count: this.state.count = 0, });
     }
 
-    incrementUp(){
-        console.log("work")
-    }
-    
-    render() { 
+    render() {
         return (
-        <div>
-            <span className={this.getBadgeClasses()}> {this.formatCount()} </span> 
-            <button onClick={this.incrementUp}  className="btn btn-secondary btn-sm">Increment</button> 
-            <ul>
-                { this.state.tags.map(tag => <li key={tag}> { tag } </li>) }
-            </ul>
-        </div>
+
+            <div className="ml-20 mx-96 mt-20">
+                <div className="grid grid-cols-12">
+                    <div className=" inline-block  col-span-1">
+                        <span className=" bg-yellow-400 rounded-sm py-1 px-2  text-center font-bold ">{this.state.count} </span>
+                    </div>
+                    <div className="col-span-11 flex inline-block">
+                        <button onClick={this.increment} className="px-2 mr-2 focus:outline-none bg-blue-400 text-white font-bold rounded-sm">+</button>
+                        <button onClick={this.substraction} className="px-2 mx-2 focus:outline-none font-bold bg-green-400 text-white rounded-sm">-</button>
+                        <button onClick={this.resetRow} className="px-4 py-1 my-auto mx-2 focus:outline-none font-bold text-xs bg-red-400 text-white rounded-sm">Reset</button>
+                    </div>
+                </div>
+            </div>
 
         );
     }
 
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
 
-    formatCount(){
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
 }
 
 
--- src/index.js
+++ src/index.js
@@ -4,14 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Counter />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Counters />, document.getElementById('root'));
 
 reportWebVitals();
